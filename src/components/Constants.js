40cd1c714b99a5328efde96ea661c1790304155b
Gattoji <lokesh-chary.gattoji@capgemini.com>
2024-11-12 00:30:48 +0530

    Added Homepage Images and mobilescreen images

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,37 +1,22 @@
 export const backimages = [
   {
     name: "Shoes",
-    banner:
-      "https://www.fansarmy.in/cdn/shop/files/Copy_of_Copy_of_Untitled_26.jpg?v=1692963922",
+    banner: "animeBackImages/coverbg/Shoes.webp",
   },
   {
     name: "Tokyo Revengers",
-    banner:
-      "https://www.kiayaaccessories.com/cdn/shop/files/Kiaya-Home-Page-Banner-01-min.webp?v=1680929585&width=2000",
+    banner: "animeBackImages/coverbg/tokyorevengers.webp",
   },
   {
     name: "Hoodies",
-    banner:
-      "https://www.fansarmy.in/cdn/shop/files/Copy_of_Copy_of_Untitled_23.jpg?v=1686477725",
-  },
-  {
-    name: "Caps",
-    banner:
-      "https://www.kiayaaccessories.com/cdn/shop/files/Kiaya-Home-Page-Banner-09.webp?v=1676894942&width=2000",
-  },
-  {
-    name: "Bags",
-    banner:
-      "https://www.kiayaaccessories.com/cdn/shop/files/Web-banner_2.jpg?v=1685992823&width=2000",
+    banner: "animeBackImages/coverbg/Hoodies.webp",
   },
   {
     name: "OverSized-T-Shirts",
-    banner:
-      "https://www.fansarmy.in/cdn/shop/files/Copy_of_Copy_of_Untitled_22.jpg?v=1683357929",
+    banner: "animeBackImages/coverbg/Oversized-T-Shirts.webp",
   },
   {
     name: "OverSized-T-Shirts",
-    banner:
-      "https://www.fansarmy.in/cdn/shop/files/Copy_of_Copy_of_Untitled_21.jpg?v=1679305970",
+    banner: "animeBackImages/coverbg/Oversized-T-Shirts2.webp",
   },
 ];
--- src/components/MainSlider.js
+++ src/components/MainSlider.js
@@ -9,44 +9,7 @@ import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "./MainSlider.css";
 import { Link } from "react-router-dom";
-
-const backimages = [
-  {
-    name: "Shoes",
-    banner:
-      "https://www.fansarmy.in/cdn/shop/files/Copy_of_Copy_of_Untitled_26.jpg?v=1692963922",
-  },
-  {
-    name: "Tokyo Revengers",
-    banner:
-      "https://www.kiayaaccessories.com/cdn/shop/files/Kiaya-Home-Page-Banner-01-min.webp?v=1680929585&width=2000",
-  },
-  {
-    name: "Hoodies",
-    banner:
-      "https://www.fansarmy.in/cdn/shop/files/Copy_of_Copy_of_Untitled_23.jpg?v=1686477725",
-  },
-  {
-    name: "Caps",
-    banner:
-      "https://www.kiayaaccessories.com/cdn/shop/files/Kiaya-Home-Page-Banner-09.webp?v=1676894942&width=2000",
-  },
-  {
-    name: "Bags",
-    banner:
-      "https://www.kiayaaccessories.com/cdn/shop/files/Web-banner_2.jpg?v=1685992823&width=2000",
-  },
-  {
-    name: "OverSized-T-Shirts",
-    banner:
-      "https://www.fansarmy.in/cdn/shop/files/Copy_of_Copy_of_Untitled_22.jpg?v=1683357929",
-  },
-  {
-    name: "OverSized-T-Shirts",
-    banner:
-      "https://www.fansarmy.in/cdn/shop/files/Copy_of_Copy_of_Untitled_21.jpg?v=1679305970",
-  },
-];
+import { backimages } from "./Constants";
 
 function MainSlider() {
   return (
